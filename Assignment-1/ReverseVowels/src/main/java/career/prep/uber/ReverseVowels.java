338def50f42f8978f4b722cc3d7a33289463ef45
JosephBorodach <95253429+JosephBorodach@users.noreply.github.com>
2023-01-29 10:22:28 -0500

    Slightly faster solution

--- Assignment-1/ReverseVowels/src/main/java/career/prep/uber/ReverseVowels.java
+++ Assignment-1/ReverseVowels/src/main/java/career/prep/uber/ReverseVowels.java
@@ -3,14 +3,12 @@ package career.prep.uber;
 import java.util.*;
 
 public class ReverseVowels {
-    private final StringBuilder sb;
-    private final Set<Character> vowels;
+    private String s;
 
     /**
      * Time: O(n)
      * Space: O(n)
      * Is not in place
-     * Is stable
      * ~10 min to write solution
      * ~10 min to write tests
      *
@@ -20,6 +18,9 @@ public class ReverseVowels {
      * b) Could have use a char array but was less clean of a solution
      * c) Could have chosen between upper and lowercase instead of adding both to set,
      * but that seemed unnecessarily complicated.
+     * d) Originally, I used a set of vowels to check if a given char was a vowel, but then I realized that
+     *      (1) it would be faster to just use an if statement and
+     *      (2) I could add in an addition check to see if the given char was even a letter
      * @throws IllegalArgumentException if input is null
      * @param s
      */
@@ -27,8 +28,7 @@ public class ReverseVowels {
         if (s == null) {
             throw new IllegalArgumentException();
         }
-        this.sb = new StringBuilder(s);
-        vowels = new HashSet<>(Arrays.asList('A', 'a', 'E', 'e', 'I', 'i', 'O', 'o', 'U', 'u'));
+        this.s = s;
 
     }
 
@@ -36,14 +36,64 @@ public class ReverseVowels {
      * find left vowel
      * find right vowel
      * swap them, and update both pointers
+     * Notes:
+     * a) 3 ways to convert char[] to string: char[] a = {'a', 'b'};
+     * 1) new String(a)
+     * 2) String.valueOf(a)
+     * 3) String.copyValueOf(a)
+     *
      * @return
      * If input is empty, the for loop won't get off the ground.
      */
     public String solveIt() {
-        for (int L = 0, R = sb.length() - 1; L < R; ) {
-            if (!vowels.contains(sb.charAt(L))) {
+        char[] chars = s.toCharArray();
+        for (int L = 0, R = s.length() - 1; L < R; ) {
+            if (!isVowel(chars[L])) {
                 L++;
-            } else if (!vowels.contains(sb.charAt(R))) {
+            } else if (!isVowel(chars[R])) {
+                R--;
+            } else {
+                char temp = chars[L];
+                chars[L++] = chars[R];
+                chars[R--] = temp;
+            }
+        }
+        return String.valueOf(chars);
+    }
+
+    /**
+     * Character.isLetter(c) and isAlphabetic would not include things like periods
+     * @param c
+     * @return
+     */
+    public static boolean isVowel(char c) {
+        return (c == 'a' || c == 'e' ||c == 'i' || c == 'o'||c == 'u' || c == 'A'|| c == 'E'||c == 'I' || c == 'O' || c=='U');
+    }
+
+    /**
+     * The methods below are not used
+     * @return
+     */
+    public String solveIt1() {
+        char[] chars = s.toCharArray();
+        for (int L = 0, R = s.length() - 1; L < R; ) {
+            while (L < R && !isVowel(chars[L])) L++;
+            while (R > L && !isVowel(chars[R])) R--;
+            if (L < R) {
+                char temp = chars[L];
+                chars[L++] = chars[R];
+                chars[R--] = temp;
+            }
+        }
+        return String.valueOf(chars);
+    }
+
+    public String solveIt2() {
+        StringBuilder sb = new StringBuilder(s);
+        for (int L = 0, R = sb.length() - 1; L < R; ) {
+            if (!isVowel(sb.charAt(L))) {
+                L++;
+            } else if (!isVowel(sb.charAt(R))) {
                 R--;
             } else {
                 char temp = sb.charAt(L);
@@ -52,5 +102,28 @@ public class ReverseVowels {
             }
         }
         return sb.toString();
+    }
+
+    /**
+     * Time: O(n^2)
+     * In the end, in terms of space this solution was not better
+     * @return
+     */
+    public String solveIt3() {
+        int len = s.length();
+        for (int L = 0, R = len - 1; L < R; ) {
+            if (!isVowel(s.charAt(L))) {
+                L++;
+            } else if (!isVowel(s.charAt(R))) {
+                R--;
+            } else {
+                char left = s.charAt(L);
+                char right = s.charAt(R);
+                s = s.substring(0, L) + right + s.substring(L+1, R) + left + s.substring(R+1, len);
+                L++;
+                R--;
+            }
+        }
+        return s;
     }
 }
