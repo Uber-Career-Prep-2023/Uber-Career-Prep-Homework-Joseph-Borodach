cd5f1c1c8a4f0597d66afb16d938b4a3b17f78eb
JosephBorodach <95253429+JosephBorodach@users.noreply.github.com>
2023-03-30 13:12:28 -0400

    Made performance improvement

--- Assignment-2/LeftView/src/main/java/career/prep/uber/LeftView.java
+++ Assignment-2/LeftView/src/main/java/career/prep/uber/LeftView.java
@@ -1,5 +1,8 @@
 package career.prep.uber;
 
+import java.util.List;
+import java.util.Queue;
+import java.util.ArrayList;
 import java.util.LinkedList;
 
 /**
@@ -21,113 +24,122 @@ import java.util.LinkedList;
  *              a. To avoid the overhead of recursion
  *              b. It's more challenging
  *
- * General Solution: Perform in-order dfs, only adding left nodes to the array.
+ * General Solution: Perform iterative bfs: Add the first node in the level to the array
  *
- * Pseudo Code:
- *      1. Set up:
- *          Find the height of the tree:
- *              Use iterative bfs to find the height of the tree.
- *          Create an array of the height of the tree.
- *      2. Perform bfs:
+ * Pseudo-code:
+ *      1. Add root to queue
+ *      2. Iterate over every level. For each:
+ *          a. Add first node in queue to the left most view list.
+ *          b. Dequeue every node in the queue, removing all nodes for the current level
+ *          c. Enqueue all nodes on the next level
+ *      3. Convert list to array.
+ *
+ * @time: O(n + h) = O(n)
+ *          where:
+ *              n is the number of nodes in tree
+ *              h is the height of the tree
+ *          This is because:
+ *              n for iterating over the tree one time, immediately adding the nodes to a list
+ *              h, converting the list to an array
+ *
+ * @Space: O((h * 2) + h):
+ *      where h is the height of the tree.
+ *      a. (h * 2) for the queue at the bottom level of the tree,
+ *          The number of nodes at any level of a full tree is that level * 2 (except for the first level).
+ *      b. h for the size of the list which had to be converted to an array.
+ *
+ * Note: The previous implementation of this class used a two-pass approach:
+ *      1. Find the height of the tree using iterative bfs.
+ *      2. Create an array of the height of the tree.
+ *      3. Perform bfs, A SECOND TIME:
  *          Add the first node in the level to the array
  *
- * @time: O(n + n) = O(n), the number of nodes in the tree.
- *      The tree is iterated over two times: Once to get a height and a second time to get the leftmost nodes for each level.
+ *      Performance was:
+ *          time: O(n + n) = O(n)
+ *              Because the tree was iterated over twice:
+ *                  1. To get the height
+ *                  2. To add the left most nodes to the array
+ *          space: O(2 * (h * 2))
+ *              where h is the height of the tree.
+ *              (h * 2) for the queue at the bottom level of the tree,
+ *                  The number of nodes at any level of a full tree is that level * 2 (except for the first level).
+ *               2 * (h * 2) because two queues were used total.
  *
- *      If the problem asked to return a list:
- *          a. Would not need to search the tree two times.
- *          b. Could use dfs, and avoid searching the bottom right side of the tree unnecessarily.
- *          However, neither of the changes would the Big O runtime of the program.
+ *               Question: Am I technically correct because the usage of the queues did not overlap?!
  *
- * @Space: O(2 * (h * 2)):
- *      Excluded the space for the return in array
- *      Two queue in total are used.
- *          Each queue's will be most full at the bottom level of the tree.
+ *      Realized it was technically more time & space efficient (NOT Big O):
+ *          To add the left most nodes to a list as I discovered the tree the 1st time,
+ *          then convert the list to an array.
+ *
  */
 public class LeftView implements LeftViewI {
-
     /**
-     * Create and return an array of the leftmost view of the tree
-     *
-     * @param root of the binary tree
+     * Creates and returns an array of the leftmost view of the tree.
+     * @param root the root node of the binary tree
      * @return an array of the leftmost view of the tree
      * @throws IllegalArgumentException if the root is null
      */
     public int[] solveIt(Node root) {
         if (root == null) {
-            throw new IllegalArgumentException();
+            throw new IllegalArgumentException("Tree cannot be empty.");
         }
-        int h = getHeight(root);
-        return getLeftMost(root, h);
+        List<Integer> list = getLeftMostValues(root);
+        return toArray(list);
     }
 
-    /**
-     * If we want to get the right most view, just use a dequeue.
-     *
-     * @param root of the binary tree
-     * @param height of the tree
-     * @return the array filled with the leftmost values in the tree
-     */
-    private int[] getLeftMost(Node root, int height) {
-        int[] array = new int[height];
-        LinkedList<Node> queue = new LinkedList<>();
-        queue.add(root);
-        for (int h = 0; h < height; h++) {
 
-            // Add the first node in the level (left most node) to array
-            array[h] = queue.peek().val;
-
-            // dequeue every node in the queue, removing all nodes from previous level
-            // enqueue all nodes on the next level
-            int nodeCount = queue.size();
-            for (int i = 0; i < nodeCount; i++) {
-                Node parent = queue.poll();
-                addChildren(queue, parent);
-            }
-        }
-        return array;
-    }
 
     /**
-     * Use iterative bfs to find the height of the tree
+     * Performs iterative BFS to traverse the tree and add the leftmost node in each level to a list.
      *
-     * @param root of the binary tree
-     * @return the height of the tree
+     * 1. Add root to queue
+     * 2. Iterate over every level. For each:
+     *      a. Add first node in queue to the left most view list.
+     *      b. Dequeue every node in the queue, removing all nodes for the current level
+     *      c. Enqueue all nodes on the next level
+     *
+     * @param root the root node of the binary tree
+     * @return a list of the leftmost nodes in each level of the tree
      */
-    private int getHeight(Node root) {
-        LinkedList<Node> queue = new LinkedList<>();
-        queue.add(root);
-        int height = 0;
+    private List<Integer> getLeftMostValues(Node root) {
+        List<Integer> list = new ArrayList<>();
+        Queue<Node> queue = new LinkedList<>();
+        queue.offer(root);
         while (!queue.isEmpty()) {
-            height++;
-            // dequeue every node in the queue, removing all nodes from previous level
-            // enqueue all nodes on the next level
-            int nodeCount = queue.size();
-            for (int i = 0; i < nodeCount; i++) {
+            list.add(queue.peek().val);
+
+            int nodesOnLevel = queue.size();
+            for (int i = 0; i < nodesOnLevel; i++) {
                 Node parent = queue.poll();
-                addChildren(queue, parent);
+                add(queue, parent.left);
+                add(queue, parent.right);
             }
         }
-        return height;
+        return list;
     }
 
     /**
-     * @param queue
-     * @param parent
+     * If the given node is not null, adds it to the queue.
+     * @param queue the queue to which the node should be added
+     * @param node the node to add to the queue
      */
-    private void addChildren(LinkedList<Node> queue, Node parent) {
-        add(queue, parent.left);
-        add(queue, parent.right);
-    }
-
-    /**
-     * If the given node is not null, add it to the queue.
-     * @param queue
-     * @param node
-     */
-    private void add(LinkedList<Node> queue, Node node) {
+    private void add(Queue<Node> queue, Node node) {
         if (node != null) {
             queue.offer(node);
         }
+    }
+
+    /**
+     * Converts a list of integers to an array of integers.
+     * @param list the list to convert
+     * @return an array of integers
+     */
+    private int[] toArray(List<Integer> list) {
+        int size = list.size();
+        int[] array = new int[size];
+        for (int i = 0; i < size; i++) {
+            array[i] = list.get(i);
+        }
+        return array;
     }
 }
