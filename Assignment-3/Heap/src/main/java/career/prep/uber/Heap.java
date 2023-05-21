7907b8d640e992a65694753aad4f18f2db0c30ba
JosephBorodach <95253429+JosephBorodach@users.noreply.github.com>
2023-05-21 16:44:25 -0400

    Setup class for Heap

---
+++ Assignment-3/Heap/src/main/java/career/prep/uber/Heap.java
@@ -0,0 +1,35 @@
+package career.prep.uber;
+
+/**
+ * Question 2: Write a min heap class according to the following API using an array as the underlying data structure.
+ *      (A max heap has the same implementation; you simply need to flip the direction of the comparators.)
+ *      For simplicity, you can assume that the heap holds integers rather than generic comparables.
+ *
+ *
+ */
+public class Heap {
+    private int[] arr;
+
+    /**
+     * returns the min (top) element in the heap
+     */
+    public int top() {
+
+        return -1;
+    }
+
+    /**
+     * adds int x to the heap in the appropriate position
+     * @param x
+     */
+    public void insert(int x) {
+
+    }
+
+    /**
+     * removes the min (top) element in the heap
+     */
+    public void remove() {
+
+    }
+}
---
+++ Assignment-3/Heap/src/test/java/career.prep.uber/HeapTests.java
@@ -0,0 +1,20 @@
+package career.prep.uber;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.Map;
+import java.util.Set;
+
+import static org.junit.Assert.*;
+
+/**
+ * JUnit 5 test class to test the {@link Heap} class.
+ */
+public class HeapTests {
+
+    @Test
+    public void simpleTest() {
+        new Heap();
+    }
+}
