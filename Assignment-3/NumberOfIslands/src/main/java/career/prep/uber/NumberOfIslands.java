7143c1e65038079800a5c19671ac5ce3595b6a05
JosephBorodach <95253429+JosephBorodach@users.noreply.github.com>
2023-05-23 09:56:15 -0400

    Added documentation

--- Assignment-3/NumberOfIslands/src/main/java/career/prep/uber/NumberOfIslands.java
+++ Assignment-3/NumberOfIslands/src/main/java/career/prep/uber/NumberOfIslands.java
@@ -1,6 +1,14 @@
 package career.prep.uber;
 
 /**
+ * Question 4:
+ *      Given a binary matrix in which 1s represent land and 0s represent water.
+ *      Return the number of islands (contiguous 1s surrounded by 0s or the edge of the matrix).
+ *
+ * Approaches:
+ * 1.
+ *
+ * 2.
  *
  */
 public class NumberOfIslands {
