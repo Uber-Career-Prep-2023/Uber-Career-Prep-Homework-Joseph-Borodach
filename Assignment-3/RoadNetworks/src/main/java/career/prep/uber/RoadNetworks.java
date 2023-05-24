8bb0f887bea1f5492abd7c2443e54a923fda7d7f
JosephBorodach <95253429+JosephBorodach@users.noreply.github.com>
2023-05-24 08:54:05 -0400

    Setup RoadNetworks

---
+++ Assignment-3/RoadNetworks/src/main/java/career/prep/uber/RoadNetworks.java
@@ -0,0 +1,79 @@
+package career.prep.uber;
+
+import java.util.*;
+
+/**
+ * @author josephborodach
+ *
+ * Question #6:
+ *      In some states, it is not possible to drive between any two towns because they are not connected to the same road network.
+ *      Given a list of towns and a list of pairs representing roads between towns, return the number of road networks.
+ *      (For example, a state in which all towns are connected by roads has 1 road network, and a state in which none of the towns are connected by roads has 0 road networks.)
+ *
+ * Model for the Problem:
+ *      - The problem can be modeled as a Undirected Edge Graph,
+ *      where towns are verticies and roads are edges.
+ *      - The question essentially asks: How many forests or disconnected graphs are there in the network?
+ *
+ * Approaches:
+ *
+ *
+ */
+public class RoadNetworks {
+
+    // Like an edge
+    public class Pair {
+        public String town1;
+        public String town2;
+
+        public Pair(String town1, String town2) {
+
+        }
+    }
+
+    public int solveIt(List<String> towns, List<Pair> roads) {
+
+    }
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ Assignment-3/RoadNetworks/src/test/java/career/prep/uber/RoadNetworksTests.java
@@ -0,0 +1,29 @@
+package career.prep.uber;
+
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * @author josephborodach
+ *
+ * JUnit 5 test class to test the {@link RoadNetworks} class.
+ */
+public class RoadNetworksTests {
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
